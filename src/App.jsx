f6bd19e263a80ab6f260fca40904e9a25a118e7a
Oliver Patrick <oliverpatrick@olivers-air.home>
2023-07-19 10:38:13 +0100

    Added an Error Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
 import SingleArticle from './components/SingleArticle'
+import ErrorPage from './components/ErrorPage'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/articles' element={<Home />} />
         <Route path='/articles/:id' element={<SingleArticle />} />
         <Route path='/articles/topic/:topic' element={<Home />} />
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
     </div>
   )
